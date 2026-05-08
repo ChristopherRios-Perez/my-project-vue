7687344f5116268f55fcf9cace59737f07a8abdf
ChristopherRios-Perez <christpherr308@gmail.com>
2026-05-07 22:45:30 -0500

    updated Index HTML to match video

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import '@/assets/style.css'
 import router from '@/router'
 import App from '@/App.vue'
 
 createApp(App).use(router).mount('#app')
+App.use(createPinia())
+App.use(router)
+App.mount('#app')
